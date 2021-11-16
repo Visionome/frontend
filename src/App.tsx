2836b171a4dba243d99f262da5e62ba489ac9be0
Joey Giordano <joeyg@Joeys-MacBook-Air.local>
2021-11-15 19:38:50 -0500

    added visualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import './App.scss';
+import { Canvas } from '@react-three/fiber';
+import Chromosome from './components/Chromosome';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
 
 function App(): JSX.Element {
+  const fov = window.innerHeight / window.screen.height;
+  const aspect = window.innerWidth / window.innerHeight;
+  // const fov = 45;
+  // const aspect = 2;
+  // const near = 0.1;
+  // const far = 100;
+  //const camera = new THREE.PerspectiveCamera(fov, aspect);
+  //camera.position.set(0, 20, 0);
+  // <ambientLight intensity={0.5} />
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Canvas>
+      <OrbitControls
+        addEventListener={undefined}
+        hasEventListener={undefined}
+        removeEventListener={undefined}
+        dispatchEvent={undefined}
+      />
+      <Chromosome />
+      <ambientLight intensity={0.5} />
+    </Canvas>
   );
 }
 
---
+++ src/components/Chromosome.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { extend } from '@react-three/fiber';
+//import { Position } from '@react-three/drei/helpers/Position';
+import Data from '../data.json';
+import { OrbitControls } from '@react-three/drei';
+import Cytoband from './Cytoband';
+extend({ OrbitControls });
+
+function Chromosome(): JSX.Element {
+  // Read data.json
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  //const jsonData = require('./data.json');
+  //const obj = JSON.parse(jsonData);
+  // Create list of cytobands
+  //console.log(obj);
+  // Render bands within the chromosome
+  return (
+    //position={new THREE.Vector3(0, 0, 0)}
+    <mesh position={[0, 0, -100]}>
+      {Data.map((chrom) => {
+        return (
+          <Cytoband
+            key={chrom.id}
+            id={chrom.id}
+            assembly_start={chrom.assembly_start}
+            assembly_end={chrom.assembly_end}
+            location={chrom.location}
+            name={chrom.name}
+            hue={chrom.hue}
+          />
+        );
+      })}
+    </mesh>
+  );
+}
+
+{
+  /* <mesh position={pos}>
+  <boxBufferGeometry attach="geometry" />
+  <meshLambertMaterial attach="material" color="hotpink" />
+</mesh>; */
+}
+
+export default Chromosome;
