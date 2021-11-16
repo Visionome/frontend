bfe93d8d8d24b554137a289b5ad207c71b9c30dd
dylans123 <dylanskelly@knights.ucf.edu>
2021-11-15 20:31:52 -0500

    save changes

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
+import './App.scss';
 
 ReactDOM.render(
   <React.StrictMode>
