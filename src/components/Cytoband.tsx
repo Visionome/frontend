978e8e707459c58fd80a397dc1842498086e2a2f
Joey Giordano <joeyg@Joeys-MacBook-Air.local>
2022-02-17 23:02:44 -0500

    card info appears when clicking on gene appearing in search results.

--- src/components/Cytoband.tsx
+++ src/components/Cytoband.tsx
@@ -11,6 +11,8 @@ export interface CytobandProps
   len: number;
   location: string;
   ypos: number;
+  bandSelected: string;
+  setBandSelected: React.Dispatch<React.SetStateAction<string>>;
 }
 
 // Render a cytoband, given the JSON description
@@ -19,15 +21,24 @@ function Cytoband({
   id,
   ypos,
   hue,
+  location,
+  bandSelected,
+  setBandSelected,
   ...props
 }: CytobandProps): JSX.Element {
-  const pos = new THREE.Vector3(0, ypos, -30);
-  const size = new THREE.Vector3(2, len, 10);
+  const pos = new THREE.Vector3(0, ypos, -110);
+  let size = new THREE.Vector3(4, len, 10);
+  // Account for centromere width
+  if (hue === '#ffbebe') size = new THREE.Vector3(2, len, 5);
 
   // TODO: removed ref prop from mesh, figure out if this
   // is required for selecting meshes in the future.
   // const ref = useRef<THREE.Mesh>(null!);
   const [hovered, hover] = useState(false);
+
+  function showCardInfo() {
+    setBandSelected(location);
+  }
 
   function handlePointerOver() {
     hover(true);
@@ -41,6 +52,7 @@ function Cytoband({
     console.log(
       'cytoband ' + id + ' position: [' + ypos + ', ' + (ypos - len) + ']',
     );
+    showCardInfo();
   }
 
   return (
