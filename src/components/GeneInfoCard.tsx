22d63d0af6d376a20f5a0c21f4d697decae34924
Arjun Pherwani <arjunp@knights.ucf.edu>
2022-03-03 16:39:52 -0500

    attempt to use useEffect to dynamically get cif file

--- src/components/GeneInfoCard.tsx
+++ src/components/GeneInfoCard.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from 'antd';
-// import axios from 'axios';
+import axios from 'axios';
 
 // export interface GeneInfoCardProps {
 //   selectedItem: any;
@@ -9,22 +9,26 @@ import { Card } from 'antd';
 // }
 
 function GeneInfoCard(selectedItem: any, urlString: string) {
-  //   const [proteinSelected, setProteinSelected] = useState('');
-  //   const selectedProtein = async () => {
-  //     try {
-  //       const response = await axios({
-  //         method: 'get',
-  //         url: `https://www.uniprot.org/uniprot/?query=${selectedItem.gene.toUpperCase()}+AND+reviewed:yes+AND+organism:9606&sort=score&columns=id,entry name,reviewed,protein names,genes,organism,length&format=tab&limit=1`,
-  //       });
+  const [proteinSelected, setProteinSelected] = useState('');
+  const selectedProtein = async () => {
+    try {
+      const response = await axios({
+        method: 'get',
+        url: `https://www.uniprot.org/uniprot/?query=${selectedItem.gene.toUpperCase()}+AND+reviewed:yes+AND+organism:9606&sort=score&columns=id&format=tab&limit=1`,
+      });
+      setProteinSelected(response.toString());
+      console.log(response);
+    } catch (err) {
+      console.log('Error in fetching protein');
+      console.log(err);
+    }
+  };
 
-  //       console.log(response);
-  //     } catch (err) {
-  //       console.log('Error in fetching protein');
-  //       console.log(err);
-  //     }
-  //   };
+  useEffect(() => {
+    selectedProtein();
+  }, []);
 
-  //   console.log(selectProtein);
+  console.log(selectedProtein);
 
   console.log('Selected item of type:' + selectedItem);
 
@@ -32,12 +36,17 @@ function GeneInfoCard(selectedItem: any, urlString: string) {
     <>
       <div style={{ display: 'flex' }}>
         <div style={{ position: 'relative', height: 250, width: 300 }}>
-          <pdbe-molstar
-            custom-data-format="cif"
-            custom-data-url="https://alphafold.ebi.ac.uk/files/AF-Q96NU1-F1-model_v2.cif"
-            hide-controls
-          />
+          {proteinSelected !== '' ? (
+            <pdbe-molstar
+              custom-data-format="cif"
+              custom-data-url={proteinSelected}
+              hide-controls
+            />
+          ) : (
+            <></>
+          )}
         </div>
+        <p>proteinSelected</p>
       </div>
       <Card
         hoverable
