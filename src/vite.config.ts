2bb0159dab8cb6cb8e964d9e551325df772035cd
Arjun Pherwani <arjunp@knights.ucf.edu>
2022-03-05 14:33:25 -0500

    attempt to migrate to vite

---
+++ src/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import svgrPlugin from 'vite-plugin-svgr';
+import envCompatible from 'vite-plugin-env-compatible';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: 'build',
+  },
+  plugins: [reactRefresh(), svgrPlugin(), envCompatible()],
+});
