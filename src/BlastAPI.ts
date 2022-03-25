c31341f604f822ad6ed641b92c9c355a54b6b80f
suneettipirneni <suneettipirneni@icloud.com>
2022-03-24 21:37:55 -0400

    add inital blast search functionality

---
+++ src/BlastAPI.ts
@@ -0,0 +1,36 @@
+const host = 'http://localhost:5000';
+import * as mockReponse from './mockBlast.json';
+
+export interface BlastMatch {
+  align_length: number;
+  gene_symbol: string;
+  sbjct_end: number;
+  sbjct_start: number;
+  best_evalue: number;
+  best_score: number;
+  num_hits: number;
+}
+
+export interface BlastSearchResponse {
+  matches: BlastMatch[];
+}
+
+export const BlastAPI = {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async search(sequence: string): Promise<BlastMatch[]> {
+    const payload = { sequence };
+
+    console.log(payload);
+
+    return mockReponse.matches;
+    // return await fetch(`${host}/blastsearch`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(payload),
+    // })
+    //   .then((res) => res.json())
+    //   .then((json) => (json as BlastSearchResponse).matches);
+  },
+};
--- src/components/Analyzer.tsx
+++ src/components/Analyzer.tsx
@@ -3,94 +3,129 @@ import { UploadOutlined } from '@ant-design/icons';
 import { Button, Table, Upload, Typography } from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
 import React, { useState } from 'react';
+import { BlastAPI, BlastMatch } from '../BlastAPI';
 
 const { Text } = Typography;
 
 export function Analyzer(): JSX.Element {
   const [input, setInput] = useState('');
+  const [output, setOutput] = useState<BlastMatch[]>([]);
+
+  const onRunButtonClick = async () => {
+    console.log(input);
+
+    /**
+     * Columns:
+     *
+     * alignment_length
+     * gaps
+     * expect
+     */
+    const matches = await BlastAPI.search(input);
+    setOutput(matches);
+  };
+
+  const tableColumns = [
+    {
+      title: 'Gene Symbol',
+      dataIndex: 'gene_symbol',
+      key: 'gene_symbol',
+    },
+    {
+      title: '# Hits',
+      dataIndex: 'num_hits',
+      key: 'num_hits',
+    },
+    {
+      title: 'Best Score',
+      dataIndex: 'best_score',
+      key: 'best_score',
+    },
+    {
+      title: 'Best E-Value',
+      dataIndex: 'best_evalue',
+      key: 'best_evalue',
+    },
+    {
+      title: 'Align Length',
+      dataIndex: 'align_length',
+      key: 'align_length',
+    },
+    {
+      title: 'Subject Start',
+      dataIndex: 'sbjct_start',
+      key: 'sbjct_start',
+    },
+    {
+      title: 'Subject End',
+      dataIndex: 'sbjct_end',
+      key: 'sbjct_end',
+    },
+  ];
+
   return (
     <div>
-      <div style={{ display: 'flex', alignItems: 'space-evenly' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'space-evenly',
+          flexDirection: 'column',
+        }}
+      >
+        <h1 style={{ textAlign: 'left' }}>Sequence Analyzer</h1>
+        <Text style={{ textAlign: 'left', fontSize: 14 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Sollicitudin tempor id eu nisl nunc. Mattis nunc sed blandit libero
+          volutpat sed cras ornare arcu. Elementum nisi quis eleifend quam
+          adipiscing vitae. Pellentesque pulvinar pellentesque habitant morbi
+          tristique senectus. Odio tempor orci dapibus ultrices. Proin fermentum
+          leo vel orci porta non. Vulputate sapien nec sagittis aliquam
+          malesuada bibendum. Id velit ut tortor pretium viverra. Nunc sed augue
+          lacus viverra vitae congue eu consequat ac. Semper feugiat nibh sed
+          pulvinar proin gravida. In aliquam sem fringilla ut morbi tincidunt
+          augue interdum velit. Sed lectus vestibulum mattis ullamcorper.
+          Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus.
+        </Text>
+
+        <TextArea style={{ height: 200 }} />
         <div
           style={{
+            width: '100%',
             display: 'flex',
-            flexBasis: '100%',
-            flexDirection: 'column',
+            flexDirection: 'row',
+            alignContent: 'space-between',
           }}
         >
-          <h1 style={{ textAlign: 'left' }}>Sequence Analyzer</h1>
-          <Text style={{ textAlign: 'left', fontSize: 14 }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Sollicitudin tempor id eu nisl nunc. Mattis nunc sed blandit libero
-            volutpat sed cras ornare arcu. Elementum nisi quis eleifend quam
-            adipiscing vitae. Pellentesque pulvinar pellentesque habitant morbi
-            tristique senectus. Odio tempor orci dapibus ultrices. Proin
-            fermentum leo vel orci porta non. Vulputate sapien nec sagittis
-            aliquam malesuada bibendum. Id velit ut tortor pretium viverra. Nunc
-            sed augue lacus viverra vitae congue eu consequat ac. Semper feugiat
-            nibh sed pulvinar proin gravida. In aliquam sem fringilla ut morbi
-            tincidunt augue interdum velit. Sed lectus vestibulum mattis
-            ullamcorper. Dignissim diam quis enim lobortis scelerisque fermentum
-            dui faucibus.
-          </Text>
+          <Upload>
+            <Button icon={<UploadOutlined />}>Click to Upload</Button>
+          </Upload>
+          <Button type="primary" onClick={onRunButtonClick}>
+            Run
+          </Button>
         </div>
-        <div
-          style={{ flexBasis: '100%', padding: '10px' }}
-          // @ts-expect-error
-          onChange={(dispatch) => setInput(dispatch.target.value)}
-        >
-          <TextArea style={{ height: 200 }} />
-          <div
-            style={{
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'row',
-              alignContent: 'space-between',
-            }}
-          >
-            <Upload>
-              <Button icon={<UploadOutlined />}>Click to Upload</Button>
-            </Upload>
-            <Button type="primary">Run</Button>
-          </div>
-        </div>
+        <h1 style={{ textAlign: 'left' }}>Results</h1>
+
+        <h1 style={{ textAlign: 'left' }}>Output Summary</h1>
+        <Text style={{ textAlign: 'left' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Sollicitudin tempor id eu nisl nunc. Mattis nunc sed blandit libero
+          volutpat sed cras ornare arcu. Elementum nisi quis eleifend quam
+          adipiscing vitae. Pellentesque pulvinar pellentesque habitant morbi
+          tristique senectus. Odio tempor orci dapibus ultrices. Proin fermentum
+          leo vel orci porta non. Vulputate sapien nec sagittis aliquam
+          malesuada bibendum. Id velit ut tortor pretium viverra. Nunc sed augue
+          lacus viverra vitae congue eu consequat ac. Semper feugiat nibh sed
+          pulvinar proin gravida. In aliquam sem fringilla ut morbi tincidunt
+          augue interdum velit. Sed lectus vestibulum mattis ullamcorper.
+          Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus.
+        </Text>
+        <Button type="primary">View Gene in Visualization</Button>
+        <Table dataSource={output} columns={tableColumns} />
       </div>
-      <div style={{ display: 'flex', alignItems: 'space-evenly' }}>
-        <div
-          style={{
-            display: 'flex',
-            flexBasis: '100%',
-            flexDirection: 'column',
-          }}
-        >
-          <h1 style={{ textAlign: 'left' }}>Results</h1>
-          <Table />
-        </div>
-        <div
-          style={{ flexBasis: '100%', padding: '10px' }}
-          // @ts-expect-error
-          onChange={(dispatch) => setInput(dispatch.target.value)}
-        >
-          <h1 style={{ textAlign: 'left' }}>Output Summary</h1>
-          <Text style={{ textAlign: 'left' }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Sollicitudin tempor id eu nisl nunc. Mattis nunc sed blandit libero
-            volutpat sed cras ornare arcu. Elementum nisi quis eleifend quam
-            adipiscing vitae. Pellentesque pulvinar pellentesque habitant morbi
-            tristique senectus. Odio tempor orci dapibus ultrices. Proin
-            fermentum leo vel orci porta non. Vulputate sapien nec sagittis
-            aliquam malesuada bibendum. Id velit ut tortor pretium viverra. Nunc
-            sed augue lacus viverra vitae congue eu consequat ac. Semper feugiat
-            nibh sed pulvinar proin gravida. In aliquam sem fringilla ut morbi
-            tincidunt augue interdum velit. Sed lectus vestibulum mattis
-            ullamcorper. Dignissim diam quis enim lobortis scelerisque fermentum
-            dui faucibus.
-          </Text>
-          <Button type="primary">View Gene in Visualization</Button>
-        </div>
-      </div>
+
+      <div style={{ display: 'flex', alignItems: 'space-evenly' }}></div>
     </div>
   );
 }
