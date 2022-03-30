cb63738134d9132c5944c3bd5b78aa36796652c2
Joey Giordano <joeydanodano@gmail.com>
2022-03-30 23:59:56 +0000

    attempting to merge.

--- src/components/HelpPage.tsx
+++ src/components/HelpPage.tsx
@@ -3,57 +3,55 @@ import React from 'react';
 export function HelpPage(): JSX.Element {
   return (
     <div className="HelpPage">
-      <h1 style={{ fontSize: 20, textAlign: 'center' }}>Help Page</h1>
-      <h2>What is VisoNome?</h2>
+      <h1 style={{ fontSize: 20, textAlign: 'center' }}>Help</h1>
+      <h2>What is VISIONome?</h2>
       <p>
-        Welcome to VISONome. This is a website developed by four UCF students
-        with info about the human genome. As everyone knows all humanbeings are
-        composed of DNA. However, it can become a little complex to some folks
-        who are first introduced to theconcept. That is what VISONome is for. It
-        is not an actually real word, but a combination of the words vision and
-        and genome. This website was made to combine computer aspects of
-        visualization with information on the human genome. Every person on this
-        planet has a unique form of genome sequencing, and said sequencing can
-        contain a slight difference in their genetic coding, which could result
-        with a genetic disease, specific to that difference. VisioNome helps
-        users see the
+        Welcome to VISIONome. This is a website developed by five UCF students
+        with info about the human genome. As many know, every human possesses a
+        unique DNA sequence. However, it can become extremely complex to
+        understand for those without training and background in bioinformaticxs.
+        This is what inspired us to create VISIONome. VISIONome is a portmanteau
+        of the words vision and and genome. This website was made to combine
+        computer aspects of visualization with information on the human genome.
+        Every person on this planet has a unique form of genome sequencing, and
+        said sequencing can contain a slight difference in their genetic coding,
+        which could result in genetic diseases, specific to that difference.
       </p>
 
       <h2>Goal</h2>
       <p>
-        Being a bioinformatic website, the goal of VISIONome is not to be the
-        best of the bunch. What this website offers that other do not is the
+        Being a bioinformatics-centered website, the goal of VISIONome is not to
+        be the best of the bunch. What this website offers that others do
         visualization aspect. The Human Genome is complex and can be a bit
         difficult to understand for anyone getting into the subject for the very
-        first time. VisioNome is made for the purpose of helping people, who are
-        new or new or struggle with the human genome, such as High-Schoolers.
-        Other Other websites are more scientific, and can be a bit informative,
-        at the levels where beginners can understand. Of course, this website
+        very first time. VISIONome is made for the purpose of helping people,
+        people, who are new or new or struggle with the human genome, such as
+        high-schoolers. Other websites are more technical in nature, and less
+        appropriate for the understanding of beginners. Of course, this website
         might require some basic knowledge of the human genome, but it should be
-        more simplier to understand what you want to know.
+        simple to traverse and explore in your quest for more knowledge on the
+        genome.
       </p>
 
-      <h2>How it works, and how to use</h2>
+      <h2>How to Use</h2>
       <p>
-        To get started with VisioNome, you will be presented with an ideogram of
+        To get started with VISIONome, you will be presented with an diagram of
         all twenty-four chromosomes, including the Y and X chromosomes. Each
-        chromosome on the ideogram can be viewed individually, simply by
-        clicking on it, allowing you to view the cytoband. Within said cytoband,
-        you can see the different bands that the cytoband is composed of
-        multiple different bands that you can also click, which in turn will
-        display info of that chromosome. To use the main feature of the website,
-        simply type your desired disease, into the search bar which in turn
-        light up one of the twenty-four chromosomes. Within that chromosome, it
-        will also light up the area on the cytoband showing where the disease
-        comes from.
+        chromosome on the <i>ideogram</i> can be viewed individually, simply by
+        clicking on it, allowing you to view the <i>cytobands</i> contained
+        within. To use the primary feature of the visualization, simply type
+        your desired disease or gene into the search bar, which in turn, will
+        light up search results in the chromosomes. Within any selected
+        chromosome, it will also light up the area on the cytoband showing where
+        the disease information was found.
       </p>
 
       <h2>About the creators</h2>
       <p>
-        The project started with five College Students who came together to make
-        a project together for Senior Design. One of us came up with the idea of
-        making an easy to use bioinformatic website, and we all decided to pitch
-        in and help.
+        The project started as five UCF students who came together to make a
+        project together for Senior Design. One of us came up with the idea of
+        making an easy to use bioinformatics website, and we all worked over the
+        course of nine months to make it a reality.
       </p>
     </div>
   );
